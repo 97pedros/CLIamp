aa44168a9862fab866c5548b12403ba52b101c5a
Pedro dos Santos <67605385+97pedros@users.noreply.github.com>
2020-11-17 15:23:14 -0300

    vish

--- cliamp.c
+++ cliamp.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int help();
 
 int main(int argc, char const *argv[])
 {
@@ -9,11 +8,5 @@ int main(int argc, char const *argv[])
     int impedancia = atoi(argv[2]);
     printf("%dV, %dohm \n", voltagem, impedancia);
     printf("total: %dW \n", voltagem * voltagem / impedancia);
-    help();
     return 0;
-}
-
-int help()
-{
-    printf("syntax: cliamp [volts] [impedance]\n");
 }
