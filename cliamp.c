7c4dee62e3345301e288ea6a60e4f4c2272779d0
Pedro dos Santos <67605385+97pedros@users.noreply.github.com>
2020-11-17 14:55:00 -0300

    atualizei o nome e iniciei o help

--- cliamp.c
+++ cliamp.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int help();
+
 int main(int argc, char const *argv[])
 {
     int voltagem = atoi(argv[1]);
     int impedancia = atoi(argv[2]);
     printf("%dV, %dohm \n", voltagem, impedancia);
     printf("total: %dW \n", voltagem * voltagem / impedancia);
+    help();
     return 0;
+}
+
+int help()
+{
+    printf("syntax: cliamp [volts] [impedance]\n");
 }
