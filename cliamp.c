a525225bfd20fb6bcce0f840b463f896b7f68cd2
Pedro dos Santos <67605385+97pedros@users.noreply.github.com>
2020-11-18 13:42:46 -0300

    fiz tudo que eu não devia

--- cliamp.c
+++ cliamp.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int cliamp();
 
 int main(int argc, char const *argv[])
 {
-    int voltagem = atoi(argv[1]);
-    int impedancia = atoi(argv[2]);
-    printf("%dV, %dohm \n", voltagem, impedancia);
-    printf("total: %dW \n", voltagem * voltagem / impedancia);
+    if(argc == 3)
+        {
+            int voltagem = argv[1];
+            int impedancia = argv[2];
+            cliamp(voltagem, impedancia);
+        }
+    if(argc < 3)
+    {
+        if(argv[1] == "help")
+        {
+            printf("help");
+        }
+    }
+    return 0;
+}
+
+int cliamp(int volts, int imp)
+{
+    int voltagem = atoi(volts);
+    int impedancia = atoi(imp);
+    printf("\n%dV, %dohm \n", voltagem, impedancia);
+    printf("total: %dW RMS\n", voltagem * voltagem / impedancia);
     return 0;
 }
